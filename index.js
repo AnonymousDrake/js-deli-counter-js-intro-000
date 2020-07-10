3b2e47f4c36aff62f580915831a293401900142f
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 09:19:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,7 @@ var currentLine= katzDeliLine =>{
   if(katzDeliLine.length===0){
     return `The line is currently empty.`;
   }
-  else {
-    return ("The line is currently" + for(let i=0;i<katzDeliLine.length;i+=1){
-      if(i===(katzDeliLine.length-1)) `${i+1}. ${katzDeliLine}.`
-      else `${i+1}. ${katzDeliLine},`
-    })
+  else {  
+    `The line is currently: `
   }
 }
