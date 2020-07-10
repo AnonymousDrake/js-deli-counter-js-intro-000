ff0341ed4fe90077ae268e79f940922617020693
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 09:28:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ var currentLine= katzDeliLine =>{
     return `The line is currently empty.`;
   }
   else {
-    window.print("The line is currently: ");
+    for(let i=0;i<katzDeliLine.length;i+=1){
+      if(i===(katzDeliLine.length - 1)){
+        return `${i+1}. ${katzDeliLine[i]}.`
+      }
+      else{
+        return (`${i+1}. ${katzDeliLine[i]},`)
+      }
+    }
 
   }
 }
