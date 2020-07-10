13edf768cd0fffa940e20dc490df35accf12c755
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 09:13:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ else{
   return `Currently serving ${katzDeliLine.shift()}.`;
 }
 }
+
+var currentLine= katzDeliLine =>{
+  if(katzDeliLine.length===0){
+    return `The line is currently empty.`;
+  }
+  else {
+    return ("The line is currently" + for(let i=0;i<katzDeliLine.length;i+=1){
+      if(i===(katzDeliLine.length-1)) `${i+1}. ${katzDeliLine}.`
+      else `${i+1}. ${katzDeliLine},`
+    })
+  }
+}
