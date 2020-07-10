95bf082e24cdd07931f28ce4a38408d3f319c1ae
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 08:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ else{
 
 var nowServing= katzDeliLine =>{
 if(katzDeliLine.length===0){
-  console.log(`There is nobody waiting to be served!`);
+  return `There is nobody waiting to be served!`;
 }
 else{
-  console.log(`Currently serving ${katzDeliLine[0]}`)
+  return`Currently serving ${katzDeliLine[0]}`;
 }
 }
 
