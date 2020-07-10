3addf370e2be6471565c90fba6b27e0a4fd4f73c
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 08:33:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var takeANumber = (katzDeliLine,customerName) =>{
 if(katzDeliLine.length===0)katzDeliLine[0]=customerName;
 else katzDeliLine.push(customerName);
-console.log(`Welcome, ${katzDeliLine[katzDeliLine.length]}. You are number ${katzDeliLine.length + 1} in line.`);
+console.log(`Welcome, ${katzDeliLine[katzDeliLine.length-1]}. You are number ${katzDeliLine.length} in line.`);
 }
 
 var nowServing= katzDeliLine =>{
